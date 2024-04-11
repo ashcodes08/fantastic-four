aa71a4d80bb4dfb5bc1d3211d60d3fe15526e19f
ashcodes08 <147363154+ashcodes08@users.noreply.github.com>
2024-04-11 21:59:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const paymentRoutes = require("./Routes/payment");
-const app = express();
 
-dotenv.config();
+global.foodData = require('./db')(function call(err, data, CatData) {
+  // console.log(data)
+  if(err) console.log(err);
+  global.foodData = data;
+  global.foodCategory = CatData;
+})
+const cors = require('cors');
 
-app.use(express.json());
-app.use(cors());
+const express = require('express')
+const app = express()
+const port = 5000
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.json())
 
-app.use("/api/payment/", paymentRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.use('/api', require("./Routes/CreatUser"))
+app.use('/api', require("./Routes/DisplayData"))
+app.use('/api', require("./Routes/OrderData"))
+app.use('/api',require("./Routes/Products"))
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
